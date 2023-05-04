4c818994634e7c9782809b2d2e1e78fc11546b3d
Amin Saffarnejad <vira1368@gmail.com>
2023-05-04 08:50:34 +0330

    Add context.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var mongoClient = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var mongoDatabase = mongoClient.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            var mongoCollection = mongoDatabase.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
