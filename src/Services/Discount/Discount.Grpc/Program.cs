7398111a78dbf41664e152684eb592a3e9bc45b7
Amin Saffarnejad <vira1368@gmail.com>
2023-05-18 21:00:32 +0330

    Add migrate postgresql database for discount grpc.

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,4 @@
+using Discount.Grpc.Extensions;
 using Discount.Grpc.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+
+app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
