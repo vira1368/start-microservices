44895c82c140740abfe3dafaf636ffe4d588acc6
Amin Saffarnejad <vira1368@gmail.com>
2023-05-25 14:50:59 +0330

    Add GetOrdersQuery.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrders
+{
+    public class GetOrdersQuery : IRequest<List<OrderDto>>
+    {
+        public string Username { get; set; }
+
+        public GetOrdersQuery(string username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+    }
+}
