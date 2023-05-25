de1587369c3ba4b4636ffb68cc57a0af5689bd34
Amin Saffarnejad <vira1368@gmail.com>
2023-05-25 15:00:19 +0330

    Add GetOrdersQueryHandler.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrders
+{
+    public class GetOrdersQueryHandler : IRequestHandler<GetOrdersQuery, List<OrderDto>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrderDto>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrdersByUsername(request.Username);
+            return _mapper.Map<List<OrderDto>>(orders);
+        }
+    }
+}
