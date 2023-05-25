35e012a4aa3b0abdc330bd8db6a352983ca6e20e
Amin Saffarnejad <vira1368@gmail.com>
2023-05-25 13:09:11 +0330

    Fix name AddAsync method.

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -16,7 +16,7 @@ namespace Ordering.Application.Contracts.Persistence
             List<Expression<Func<T, object>>> includes = null,
             bool disableTracking = true);
         Task<T> GetByIdAsync(int id);
-        Task<T> GetAsync(T entity);
+        Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
     }
