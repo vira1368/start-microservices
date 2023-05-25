2414893b4fa9bece94c91aee8c8298c9f3bb4366
Amin Saffarnejad <vira1368@gmail.com>
2023-05-25 14:49:07 +0330

    Update mapping profile.

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrders;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+        }
     }
 }
