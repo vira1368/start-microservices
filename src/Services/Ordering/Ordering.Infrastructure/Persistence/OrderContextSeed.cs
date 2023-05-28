bb993a156c8103798bfa5ec6bfdbc3efddcf4d41
Amin Saffarnejad <vira1368@gmail.com>
2023-05-28 11:32:24 +0330

    Add OrderContextSeed.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Seed database associated with context {typeof(OrderContext).Name}");
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders() => new List<Order>
+            {
+                new Order
+                {
+                    Username = "vira",
+                    FirstName = "Amin",
+                    LastName = "Saffarnejad",
+                    EmailAddress = "vira1368@gmail.com",
+                    AddressLine = "Tehran",
+                    Country = "Iran",
+                    TotalPrice = 350
+                }
+            };
+    }
+}
