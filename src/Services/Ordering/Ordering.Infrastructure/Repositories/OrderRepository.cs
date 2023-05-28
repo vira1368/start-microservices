3b688aa987cdf9817db7e2bd87c6904021ab21ff
Amin Saffarnejad <vira1368@gmail.com>
2023-05-28 13:45:07 +0330

    Add OrderRepository.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUsername(string username)
+        {
+            var orders = await _context.Orders
+                .Where(order => order.Username == username)
+                .ToListAsync();
+            return orders;
+        }
+    }
+}
