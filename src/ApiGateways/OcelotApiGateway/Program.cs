e5eab68867f4b38f5766f60ace7eb84a06174554
Amin Saffarnejad <vira1368@gmail.com>
2023-06-11 16:12:13 +0330

    Configure logging.

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -1,4 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging((hostBuilderContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostBuilderContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
