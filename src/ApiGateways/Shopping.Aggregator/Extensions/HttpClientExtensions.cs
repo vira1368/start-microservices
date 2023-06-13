108aa47c3a5281bd3bc13ba31ee22a7cfc946ce9
Amin Saffarnejad <vira1368@gmail.com>
2023-06-14 00:57:57 +0330

    Add HttpClientExtensions.

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage httpResponseMessage)
+        {
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Somthing went wrong calling the API: {httpResponseMessage.ReasonPhrase}");
+            }
+
+            var dataAsString = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
