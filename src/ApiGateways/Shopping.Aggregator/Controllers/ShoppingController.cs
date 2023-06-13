8911e6a80406752dba4974d11957f5aa7148de39
Amin Saffarnejad <vira1368@gmail.com>
2023-06-14 00:58:09 +0330

    Add ShoppingController.

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+using System.Net;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+        {
+            _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+        }
+
+        [HttpGet("{username}", Name = "GetShopping")]
+        [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string username)
+        {
+            var basket = await _basketService.GetBasketByUsername(username);
+            foreach (var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalogById(item.ProductId);
+
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+            var orders = await _orderService.GetOrdersByUsername(username);
+
+            var shoppingModel = new ShoppingModel
+            {
+                Username = username,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+            return Ok(shoppingModel);
+        }
+    }
+}
