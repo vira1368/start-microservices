96aecdaae4407ee7a8153347c481fbd484f11a53
Amin Saffarnejad <vira1368@gmail.com>
2023-06-14 00:43:46 +0330

    Add ICatalogService.

---
+++ src/ApiGateways/Shopping.Aggregator/Services/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalogs();
+        Task<IEnumerable<CatalogModel>> GetCatalogsByCategory(string category);
+        Task<CatalogModel> GetCatalogById(string id);
+    }
+}
