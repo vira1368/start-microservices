6dd69515eab0621cdfe95e535d9a8b041573f845
Amin Saffarnejad <vira1368@gmail.com>
2023-06-14 00:56:04 +0330

    Add BasketService.

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,21 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<BasketModel> GetBasketByUsername(string username)
+        {
+            var httpResponseMessage = await _httpClient.GetAsync($"/api/v1/Basket/{username}");
+            return await httpResponseMessage.ReadContentAs<BasketModel>();
+        }
+    }
+}
