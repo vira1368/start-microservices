61219e398809bf4c01894ac6681d95053f955bc7
Amin Saffarnejad <vira1368@gmail.com>
2023-06-14 00:56:35 +0330

    Add OrderService.

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUsername(string username)
+        {
+            var httpResponseMessage = await _httpClient.GetAsync($"/api/v1/Order/{username}");
+            return await httpResponseMessage.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
