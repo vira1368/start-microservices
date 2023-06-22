ba17b3248b8de9e82c1896f695f44fe0a0936bda
Ehsan Jahangard <ehsanjahangard2020@gmail.com>
2023-06-23 00:42:04 +0330

    approve Swagger UI...

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,6 +1,8 @@
 ﻿using Catalog.API.Entities;
 using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Net;
 
 namespace Catalog.API.Controllers
@@ -17,18 +19,32 @@ namespace Catalog.API.Controllers
             _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
+        //swagger code
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [SwaggerOperation(Summary = "لیست کالا ها", Description = "لیست کالا ها را برمیگرداند، نیاز به اعتباری سنجی نیست...", Tags = new string[] { "کالا ها" })]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 500)]
+        [ProducesResponseType(typeof(string), 401)]
 
-        [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [HttpGet("GetProducts")]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
             var products = await _productRepository.GetProducts();
             return Ok(products);
         }
+        //swagger code
+        [ApiExplorerSettings(IgnoreApi = false)]
+        [SwaggerOperation(Summary = "لیست کالا براساس کد کد کالا", Description = "یک کالا را براساس کد کالا برمیگرداند، نیاز به اعتباری سنجی نیست...", Tags = new string[] { "کالا ها" })]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 500)]
+        [ProducesResponseType(typeof(string), 401)]
 
-        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+
+
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
         public async Task<ActionResult<Product>> GetProductById(string id)
         {
             var product = await _productRepository.GetProductById(id);
@@ -49,22 +65,40 @@ namespace Catalog.API.Controllers
             return Ok(products);
         }
 
-        [HttpPost]
+        //swagger code
+        [ApiExplorerSettings(IgnoreApi = false)]
+        [SwaggerOperation(Summary = "لیست کالا ها", Description = "ایجاد کالا ...", Tags = new string[] { "کالا ها" })]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 500)]
+        [ProducesResponseType(typeof(string), 401)]
+
+        [EnableCors("MyPolicy")]
+        [HttpPost("CreateProduct")]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
         {
             await _productRepository.CreateProduct(product);
             return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
         }
-
-        [HttpPut]
+        //swagger code
+        [ApiExplorerSettings(IgnoreApi = false)]
+        [SwaggerOperation(Summary = "ویرایش کالا ", Description = "ویرایش کالا را انجام میدهد...", Tags = new string[] { "کالا ها" })]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 500)]
+        [ProducesResponseType(typeof(string), 401)]
+        [HttpPut("UpdateProduct")]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        public async Task<IActionResult> UpdateProduct([FromForm] Product product)
         {
             var isUpdate = await _productRepository.UpdateProduct(product);
             return Ok(isUpdate);
         }
-
+        //swagger code
+        [ApiExplorerSettings(IgnoreApi = false)]
+        [SwaggerOperation(Summary = "حذف کالا ", Description = "حذف کالا را انجام میدهد...", Tags = new string[] { "کالا ها" })]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 500)]
+        [ProducesResponseType(typeof(string), 401)]
         [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteProductById(string id)
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,6 @@
-using Catalog.API.Data;
+﻿using Catalog.API.Data;
 using Catalog.API.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +9,46 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//UI Doc - ehsan.jahnagard
+builder.Services.AddSwaggerGen(options =>
+{
+    options.EnableAnnotations();
+    options.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Version = "نسخه 1",
+        Title = "لیست کالا ها",
+        Description = "وب سرویس فروشگاه"
+    });
+
+    ////JWT Init
+    //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    //{
+    //    Scheme = "Bearer",
+    //    BearerFormat = "JWT",
+    //    In = ParameterLocation.Header,
+    //    Name = "Authorization",
+    //    Description = "Bearer Authentication with JWT Token",
+    //    Type = SecuritySchemeType.Http
+    //});
+    //options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    //    {
+    //        new OpenApiSecurityScheme {
+    //            Reference = new OpenApiReference {
+    //                Id = "Bearer",
+    //                    Type = ReferenceType.SecurityScheme
+    //            }
+    //        },
+    //        new List < string > ()
+    //    }
+    //});
+});
+
 
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
-
+app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
